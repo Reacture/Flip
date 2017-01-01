39b22ed2b441743a0508c9aa05f8defe595cd31b
Gyuwon Yi <gyuwon@live.com>
2017-01-02 01:41:33 +0900

    ModelBase<TIdentifier> 클래스룰 추가합니다.

---
+++ source/Flip.Core/ModelBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Flip
+{
+    using System;
+
+    public class ModelBase<TIdentifier> : IModel<TIdentifier>
+        where TIdentifier : IEquatable<TIdentifier>
+    {
+        protected ModelBase(TIdentifier id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            Id = id;
+        }
+
+        public TIdentifier Id { get; }
+    }
+}
---
+++ source/Flip.Tests/ModelBase_features.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Flip
+{
+    [TestClass]
+    public class ModelBase_features
+    {
+        public class FakeModel : ModelBase<Guid>
+        {
+            public FakeModel(Guid id)
+                : base(id)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void sut_implements_IModel()
+        {
+            var sut = new FakeModel(Guid.NewGuid());
+            sut.Should().BeAssignableTo<IModel<Guid>>();
+        }
+    }
+}
