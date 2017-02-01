639768b7abd02582e8b2814aa8a6a398bbbb38b7
Gyuwon Yi <gyuwon@live.com>
2017-02-02 01:43:40 +0900

    StreamExtensions 클래스를 추가합니다.

---
+++ source/Flip.Core/StreamExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Flip
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using System.Threading.Tasks;
+
+    public static class StreamExtensions
+    {
+        public static void Emit<TModel>(
+            this IConnection<TModel> connection, TModel model)
+            where TModel : class
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            if (model == null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            connection.Emit(Observable.Return(model));
+        }
+
+        public static void Emit<TModel>(
+            this IConnection<TModel> connection, Task<TModel> task)
+            where TModel : class
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            if (task == null)
+            {
+                throw new ArgumentNullException(nameof(task));
+            }
+
+            connection.Emit(task.ToObservable());
+        }
+    }
+}
---
+++ source/Flip.Tests/StreamExtensions_features.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Flip
+{
+    [TestClass]
+    public class StreamExtensions_features
+    {
+        [TestMethod]
+        public void class_has_guard_clauses()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(StreamExtensions));
+        }
+
+        public class FakeModel : ModelBase<Guid>
+        {
+            public FakeModel()
+                : base(Guid.NewGuid())
+            {
+            }
+        }
+
+        public interface IFunctor
+        {
+            void Action<T>(T arg);
+        }
+
+        [TestMethod]
+        public void Emit_relays_with_observable_with_model()
+        {
+            var model = new FakeModel();
+            var store = new StreamStore<Guid, FakeModel>();
+            var functor = Mock.Of<IFunctor>();
+            store.Connect(model.Id).Stream.Subscribe(functor.Action);
+            IConnection<Guid, FakeModel> connection = store.Connect(model.Id);
+
+            connection.Emit(model);
+
+            Mock.Get(functor).Verify(x => x.Action(model), Times.Once());
+        }
+
+        [TestMethod]
+        public void Emit_relays_observable_with_future()
+        {
+            var model = new FakeModel();
+            var store = new StreamStore<Guid, FakeModel>();
+            var functor = Mock.Of<IFunctor>();
+            store.Connect(model.Id).Stream.Subscribe(functor.Action);
+            IConnection<Guid, FakeModel> connection = store.Connect(model.Id);
+
+            connection.Emit(Task.FromResult(model));
+
+            Mock.Get(functor).Verify(x => x.Action(model), Times.Once());
+        }
+    }
+}
